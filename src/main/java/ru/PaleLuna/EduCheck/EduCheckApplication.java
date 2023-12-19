40959d27eeee13646b9eaf49c34d6d1b5dd78f95
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2023-12-19 20:06:28 +0300

    Project init
    Инициализация проекта

---
+++ src/main/java/ru/PaleLuna/EduCheck/EduCheckApplication.java
@@ -0,0 +1,13 @@
+package ru.PaleLuna.EduCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/PaleLuna/EduCheck/EduCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.PaleLuna.EduCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
